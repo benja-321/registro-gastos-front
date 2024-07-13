9f8b718296a53a57f2068b35b6b3c3b62b7df35c
benja-321 <became012@gmail.com>
2024-07-12 23:33:39 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import LoginView from "./components/LoginView.jsx";
+
+const App = () => {
+    return (
+        <LoginView/>
+    )
+};
+
+export default App;
